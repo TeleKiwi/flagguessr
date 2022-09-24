9c34b983193e2dac6711e3c62fde475cab02bd4a
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2022-09-24 18:30:47 +0100

    homepage done

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,21 @@
-<style lang="scss"> 
-    h2 {
+<script>
+    import Home from "./lib/Home.svelte";
+
+</script>
+<style>
+    #betatag {
         font-weight: 700;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        margin-left: 40px;
+        margin-bottom: 40px;
     }
+
 </style>
 
-<h1> Hello, world! </h1>
 
-<h2> BETA </h2>
+<Home></Home>
+
+
+<h2 id="betatag"> DEVELOPMENT VERSION </h2>
---
+++ src/lib/Home.svelte
@@ -0,0 +1,18 @@
+<style lang="scss">
+    #title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-block-end: 150px;
+    }
+
+    button {
+        padding: 30px
+    }
+</style>
+
+<img id="title" src="/flagguessrdark.png" alt="flagguessr">
+<button> start </button>
+
+
+
