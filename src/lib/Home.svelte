77816797d918a4241eed3dfa80a5159176c3baea
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2022-09-26 20:42:52 +0100

    i tried

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -15,12 +15,24 @@
 
 </script>
 <style lang="scss">
-    #title {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-block-end: 150px;
+    @media (min-width: 15.9rem) {
+        #title {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-block-end: 150px;
+        }
+
+        
+    } 
+
+    @media (max-width: 15.9rem ){
+        .mobile {
+            display: inline;
+        }
     }
+    
+    
 
     button {
         padding: 30px
@@ -32,9 +44,12 @@
     } */
 </style>
 
-<img id="title" src="flagguessrdark.png" alt="flagguessr">
-<button on:click|preventDefault="{startGame}"> Classic Mode </button>
-<button on:click|preventDefault="{changelog}"> View changelog </button>
+<div class="mobile">
+    <img id="title" src="flagguessrdark.png" alt="flagguessr">
+    <button on:click|preventDefault="{startGame}"> Classic Mode </button>
+    <button on:click|preventDefault="{changelog}"> View changelog </button>
+</div>
+
 
 
 <!-- <button id="frenzy" on:click|preventDefault="{warn}"> Frenzy Mode </button> -->
