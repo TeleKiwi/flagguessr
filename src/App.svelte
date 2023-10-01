39cd1adac1888ff5e35af3e9ab0da1b72d5221f9
TeleKiwi <70376250+TeleKiwi@users.noreply.github.com>
2023-10-01 15:21:56 +0100

    format

--- src/App.svelte
+++ src/App.svelte
@@ -4,16 +4,41 @@
     import Game from "./routes/Game.svelte";
     import Home from "./routes/Home.svelte";
     import Mobile from "./routes/Mobile.svelte";
-    import { highScore, lastCorrectAnswer, status } from "./stores"
+    import { highScore, lastCorrectAnswer, status } from "./stores";
 
     status.set("home");
 
-    const buildStatus = import.meta.env.DEV
+    const buildStatus = import.meta.env.DEV;
     const verNumber = import.meta.env.VITE_FLAGGUESSR_VERSION;
-    
 </script>
-<style>
 
+{#if $status == "home"}
+    <h2>{$lastCorrectAnswer}</h2>
+    {#if buildStatus}
+        <h2 id="developer">Hey there, developer!</h2>
+    {/if}
+    <Home />
+{:else if $status == "play"}
+    <Game />
+{:else if $status == "changelog"}
+    <Changelog />
+{:else if $status == "unsupported"}
+    <Mobile />
+{:else}
+    <Error />
+{/if}
+
+{#if $highScore > 0 && $status == "home"}
+    <h2>High score: {$highScore}</h2>
+{/if}
+
+{#if buildStatus}
+    <h2 id="betatag">{`${verNumber} (localhost)`}</h2>
+{:else}
+    <h2 id="betatag">{verNumber}</h2>
+{/if}
+
+<style>
     #betatag {
         font-weight: 700;
         position: fixed;
@@ -24,41 +49,10 @@
     }
 
     #developer {
-        color:aquamarine
+        color: aquamarine;
     }
 
     /* #frenzyh {
         color: red;
     } */
-
 </style>
-
-
-{#if $status == "home"}
-<h2>{$lastCorrectAnswer}</h2>
-{#if buildStatus}
-<h2 id="developer"> Hey there, developer! </h2>
-{/if}
-<Home></Home>
-{:else if $status == "play"}
-<Game></Game>
-{:else if $status == "changelog"}
-<Changelog></Changelog>
-{:else if $status == "unsupported"}
-<Mobile></Mobile>
-{:else}
-<Error></Error>
-{/if}
-
-
-
-{#if $highScore > 0 && $status == "home"}
-<h2> High score: {$highScore} </h2>
-{/if}
-
-
-{#if buildStatus}
-<h2 id="betatag"> {`${verNumber} (localhost)`} </h2>
-{:else}
-<h2 id="betatag"> {verNumber} </h2>
-{/if}
